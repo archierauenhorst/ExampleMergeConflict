e74829482c59f32c3574c043dc9828958c4e46b7
archierauenhorst <archierauenhorst@u.boisestate.edu>
2025-12-03 15:37:16 -0700

    revision two(in branch-feature)

--- App.java
+++ App.java
@@ -2,4 +2,8 @@ public class App{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public void methodInBranch(){
+        System.out.println("Method in branch");
+    }
 }
